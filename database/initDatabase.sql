a94cc43ed641ac85bd83037294488d37643aba11
Christine-Anderson <114715340+Christine-Anderson@users.noreply.github.com>
2023-07-25 10:41:03 -0700

    Add some articles to the museum

--- database/initDatabase.sql
+++ database/initDatabase.sql
@@ -39,20 +39,12 @@ CREATE TABLE Photo (
 
 CREATE TABLE Artifact (
     articleID INT PRIMARY KEY,
-    estimatedYear INT,
-    madeBy VARCHAR(50),
+    estimatedYear VARCHAR(50),
+    regionOfOrigin VARCHAR(50),
     material VARCHAR(50),
     FOREIGN KEY(articleID) REFERENCES(Article)
         ON DELETE CASCADE
         ON UPDATE CASCADE,
-    FOREIGN KEY(creator) REFERENCES(Origin)
-        ON DELETE NO ACTION
-        ON UPDATE NO ACTION
-);
-
-CREATE TABLE Origin (
-    creator VARCHAR(50) PRIMARY KEY,
-    regionOfOrigin VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE NaturalSpecimen (
@@ -125,6 +117,48 @@ CREATE TABLE Displays(
     FOREIGN KEY(articleID) REFERENCES(Article)
 );
 
--- INSERT
--- INTO Article(articleID, name, dateAquired, condition,storageLocation, UVProtection, tempControl, humidityControl)
--- VALUES ()
+-- if you want to put any items I've added on display in the exhibit, please change storageLocation to "on display"
+
+INSERT
+INTO Article(articleID, name, dateAquired, condition, storageLocation, UVProtection, tempControl, humidityControl)
+VALUES (11111, "The Forest (or The Trees)", 2021-11-05 21:45:59, "storage room 1", "Y", "Y", "Y")
+
+INSERT
+INTO Article(articleID, name, dateAquired, condition, storageLocation, UVProtection, tempControl, humidityControl)
+VALUES (11112, "William Shakespeare's Comedies, Histories, & Tragedies", 2022-01-12 13:45:31, "storage room 5", "Y", "Y", "Y")
+
+INSERT
+INTO Article(articleID, name, dateAquired, condition, storageLocation, UVProtection, tempControl, humidityControl)
+VALUES (11113, "Moonrise, Hernandez", 2018-03-03 13:45:46, "storage room 5", "Y", "Y", "Y")
+
+INSERT
+INTO Article(articleID, name, dateAquired, condition, storageLocation, UVProtection, tempControl, humidityControl)
+VALUES (11114, "The Mask of Tutankhamun", 2012-07-16 03:15:50, "storage room 3", "Y", "Y", "Y")
+
+INSERT
+INTO Article(articleID, name, dateAquired, condition, storageLocation, UVProtection, tempControl, humidityControl)
+VALUES (11115, "blue whale skeleton", 2022-10-25 09:20:18, "storage room 3", "N", "Y", "Y")
+
+INSERT 
+INTO Artwork (articleID, artist, yearMade, medium)
+VALUES (11111, "Emily Carr", 1931, "oil on canvas")
+
+INSERT 
+INTO Text (articleID, author, datePublished)
+VALUES (11112, "William Shakespeare", 1623)
+    
+INSERT 
+INTO Text (articleID, yearTaken, locationTaken)
+VALUES (11113, 1941, "Hernandez, New Mexico, USA")
+
+INSERT 
+INTO Text (articleID, estimatedYear, regionOfOrigin, material)
+VALUES (11114, "1323 BCE (New Kingdom, 18th Dynasty)", "tomb of Tutankhamun in the Valley of the Kings, Luxor, Egypt", "Gold with inlays of lapis lazuli, carnelian, quartz, obsidian, and other stones")
+
+INSERT 
+INTO Text (articleID, speciesName, timePeriod)
+VALUES (11115, "Balaenoptera musculus", "Present")
+
+INSERT 
+INTO Text (speciesName, nativeTo)
+VALUES ("Balaenoptera musculus", "all major oceans")
