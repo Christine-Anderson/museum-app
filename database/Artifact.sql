b7e81b02e2da5d08f2bb6dee51c53030edf82673
Christine-Anderson <114715340+Christine-Anderson@users.noreply.github.com>
2023-07-24 20:37:30 -0700

    Normalize Artifact.sql

--- database/Artifact.sql
+++ database/Artifact.sql
@@ -2,9 +2,11 @@ CREATE TABLE Artifact (
     articleID INT(11) PRIMARY KEY,
     estimatedYear INT(10),
     madeBy VARCHAR(50),
-    regionOfOrigin VARCHAR(50),
     material VARCHAR(50),
     FOREIGN KEY(articleID) REFERENCES(Article)
         ON DELETE CASCADE
-        ON UPDATE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY(creator) REFERENCES(Origin)
+        ON DELETE SET NULL
+        ON UPDATE NO ACTION
 );
---
+++ database/Origin.sql
@@ -0,0 +1,4 @@
+CREATE TABLE Origin (
+    creator VARCHAR(50) PRIMARY KEY,
+    regionOfOrigin VARCHAR(50) NOT NULL
+);
