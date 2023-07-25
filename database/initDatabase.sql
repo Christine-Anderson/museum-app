38e7b7e30ab71153fc3a465e7f1da8688afd0c13
Christine-Anderson <114715340+Christine-Anderson@users.noreply.github.com>
2023-07-25 15:16:10 -0700

    Insert rows into Owner,Contract, and relationships

--- database/initDatabase.sql
+++ database/initDatabase.sql
@@ -74,7 +74,7 @@ CREATE TABLE Contract (
 CREATE TABLE Owner (
     ownerID INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL,
-    buildingNum INT NOT NULL,
+    buildingNum INT,
     street VARCHAR(50) NOT NULL,
     postalOrZIPCode CHAR(7) NOT NULL,
     country VARCHAR(50) NOT NULL,
@@ -98,9 +98,9 @@ CREATE TABLE Writes(
 
 CREATE TABLE Loans(
     ownerID INT,
-    contractID INT,
+    articleID INT,
     FOREIGN KEY(ownerID) REFERENCES(Owner),
-    FOREIGN KEY(contractID) REFERENCES(Contract)
+    FOREIGN KEY(articleID) REFERENCES(Article)
 );
 
 CREATE TABLE PertainsTo(
@@ -117,17 +117,17 @@ CREATE TABLE Displays(
     FOREIGN KEY(articleID) REFERENCES(Article)
 );
 
--- if you want to put any items I've added on display in the exhibit, please change storageLocation to "on display"
+-- if you want to put any items I've added on display in the exhibit, please change storageLocation to "onDisplay"
 
 INSERT
 INTO Article(articleID, name, dateAquired, condition, storageLocation, UVProtection, tempControl, humidityControl)
 VALUES
-    (11111, "The Forest (or The Trees)", 2021-11-05 21:45:59, "Storage room 1", "Y", "Y", "Y"),  --borrow from Van art gallery
-    (11112, "William Shakespeare's Comedies, Histories, & Tragedies", 2022-01-12 13:45:31, "Storage room 5", "Y", "Y", "Y"), --borrow from UBC
-    (11114, "Tutankhamun's Mask", 2022-07-16 03:15:50, "Storage room 3", "Y", "Y", "Y"), --borrow from Egyptian Museum, Cairo, Egypt
-    (11115, "blue whale skeleton", 2022-10-25 09:20:18, "Storage room 3", "N", "Y", "Y"),
-    (11116, "replica Woolly mammoth", 2021-10-20 06:35:19, "Storage room 1", "N", "Y", "Y"), -- borrow from royal bc museum
-    (11117, "IBM cheese slicer", 2023-02-09 12:00:12, "Storage room 1", "N", "N", "N"), -- borrow from burnaby museum
+    (11111, "Forest, British Columbia", 2021-11-05 21:45:59, "Storage room 1", "Y", "Y", "Y"),
+    (11112, "William Shakespeare's Comedies, Histories, & Tragedies", 2022-01-12 13:45:31, "Storage room 5", "Y", "Y", "Y"),
+    (11114, "Tutankhamun's Mask", 2022-07-16 03:15:50, "Storage room 3", "Y", "Y", "Y"),
+    (11115, "Blue whale skeleton", 2022-10-25 09:20:18, "Storage room 3", "N", "Y", "Y"),
+    (11116, "Woolly mammoth replica", 2021-10-20 06:35:19, "Storage room 1", "N", "Y", "Y"),
+    (11117, "IBM cheese slicer", 2023-02-09 12:00:12, "Storage room 1", "N", "N", "N"),
     (11118, "Jade Death Mask of Kinich Janaab Pakal", 2017-03-25 14:40:49, "Storage room 3", "Y", "Y", "Y"), 
     (11119, "Ancient Egyptian flint arrowhead", 2023-11-08 18:45:05, "Storage room 2", "Y", "Y", "Y"),
     (11120, "Neolithic Painted Pottery", 2018-07-06 08:05:56, "Storage room 3", "Y", "Y", "Y"),
@@ -135,19 +135,19 @@ VALUES
     (11122, "Ammonite fossil", 2019-07-20 12:30:15, "Storage room 1", "N", "Y", "Y"),
     (11123, "T. rex skeleton", 2017-03-25 14:40:49, "Storage room 5", "N", "Y", "Y"),
     (11124, "Ancient Mosquito in Burmese amber", 2011-04-26 18:30:37, "Storage room 5", "N", "Y", "Y"),
-    (11125, "Mona Lisa", 2021-09-13 03:25:59, "Storage room 1", "Y", "Y", "Y"), -- borrow from  Louvre Museum, Paris, France
-    (11126, "Starry Night", 2020-07-02 12:30:15, "Storage room 1", "Y", "Y", "Y"), -- Museum of Modern Art (MoMA), New York City, USA
-    (11127, "The Scream", 2023-03-12 14:40:49, "Storage room 2", "Y", "Y", "Y"), --borrow from  National Gallery, Oslo, Norway
+    (11125, "Mona Lisa", 2021-09-13 03:25:59, "Storage room 1", "Y", "Y", "Y"),
+    (11126, "Starry Night", 2020-07-02 12:30:15, "Storage room 1", "Y", "Y", "Y"),
+    (11127, "The Scream", 2023-03-12 14:40:49, "Storage room 2", "Y", "Y", "Y"),
     (11128, "The Thinker replica", 2015-03-15 18:30:37, "Storage room 5", "N", "N", "N"), 
     (11129, "The Vancouver Court House under construction", 2018-08-17 14:12:32, "Storage room 1", "Y", "Y", "Y"),
     (11130, "The terminus of the Canadian Pacific Railway", 2018-08-17 14:12:32, "Storage room 1", "Y", "Y", "Y"),
     (11131, "A picnic in newly opened Stanley Park", 2018-08-17 14:12:32, "Storage room 1", "Y", "Y", "Y"),
     (11132, "Rebuilding Cordova Street after the Great Vancouver Fire", 2018-08-17 14:12:32, "Storage room 1", "Y", "Y", "Y"),
-    (11133, "UBC’s Main Library - now Irving K. Barber Learning Centre - under construction", 2018-08-17 14:12:32, "Storage room 1", "Y", "Y", "Y"),
-    (11134, "Papyrus of Ani – Book of the Dead", 2011-05-12 15:10:35, "Storage room 5", "Y", "Y", "Y"),
+    (11133, "UBC's Main Library - now Irving K. Barber Learning Centre - under construction", 2018-08-17 14:12:32, "Storage room 1", "Y", "Y", "Y"),
+    (11134, "Papyrus of Ani - Book of the Dead", 2022-07-16 03:15:50, "Storage room 5", "Y", "Y", "Y"),
     (11135, "Original Manuscript of Alice in Wonderland", 2013-08-07 18:55:41, "Storage room 2", "Y", "Y", "Y"),
-    (11136, "Gutenberg Bible – The Earliest Printed Book", 2014-09-28 01:25:26, "Storage room 2", "Y", "Y", "Y"),
-    (11137, "Magna Carta Libertatum – Latin for “Great Charter of Freedoms”", 2019-05-22 11:15:09, "Storage room 5", "Y", "Y", "Y"),
+    (11136, "Gutenberg Bible - The Earliest Printed Book", 2014-09-28 01:25:26, "Storage room 2", "Y", "Y", "Y"),
+    (11137, "Magna Carta Libertatum - Latin for “Great Charter of Freedoms”", 2019-05-22 11:15:09, "Storage room 5", "Y", "Y", "Y"),
     (11138, "Common Sense", 2016-03-09 10:05:08, "Storage room 2", "Y", "Y", "Y");
 
 INSERT 
@@ -202,19 +202,95 @@ INSERT
 INTO Species(speciesName, nativeTo)
 VALUES 
     ("Mammuthus primigenius", "all major oceans"),
-    ("Balaenoptera musculus", "northern Eurasia and North America"),
+    ("Balaenoptera musculus", "Northern Eurasia and North America"),
     ("Pleuroceras solare", "Canada and Europe"),
     ("Tyrannosaurus rex", "North America"),
     ("Burmaculex antiquus", "Canada");
 
--- Contract
+INSERT
+INTO Owner(ownerID, name, buildingNum, street, postalOrZIPCode, country, phoneNum)
+VALUES
+    (1111, "Vancouver Art Gallery", 750, "Hornby St", "V6Z 2H7", "Canada", "(604) 662-4700"),
+    (1112, "University of British Columbia: Library Special Collections Division", 1956, "Main Mall", "V6T 1Z2", "Canada", "(604) 822-2521"),
+    (1113, "Egyptian Museum", NULL, "El-Tahrir Square", "4272083", "Egypt", "+20 2 25796948"),
+    (1114, "Royal BC Museum", 675, "Belleville St", "V8W 9W2", "Canada", "(250) 356-7226"),
+    (1115, "Burnaby Village Museum", 6501, "Deer Lake Ave", "V5G 3T6", "Canada", "(604) 297-4565"),
+    (1116, "Richie Rich", 3085, "Point Grey Road", "V6K 1A7", "Canada", "(604) 987-6543"),
+    (1117, "Louvre Museum", NULL, "Rue de Rivoli", "75001", "France", "+33 1 40 20 53 17"),
+    (1118, "The Museum of Modern Art", 11, "W 53rd St", "10019", "United States", "+1 212-708-9400"),
+    (1119, "The National Museum of Art", 675, "Belleville St", "V8W 9W2", "Canada", "(250) 356-7226");
 
--- Owner
+INSERT
+INTO PostalCode(postalOrZIPCode, city)
+VALUES
+    ("V6Z 2H7", "Vancouver"),
+    ("V6T 1Z2", "Vancouver"),
+    ("4272083", "Cario"),
+    ("V8W 9W2", "Victoria"),
+    ("V5G 3T6", "Burnaby"),
+    ("V6K 1A7", "Vancouver"),
+    ("75001", "Paris"),
+    ("10019", "New York City"),
+    ("V8W 9W2", "Victoria");
 
--- Writes
+INSERT
+INTO Contract(contractID, startDate, endDate, text)
+VALUES
+    (1000, 2021-10-25, 2024-10-25, "The Vancouver Art Gallery will loan Forest, British Columbia to the museum from 2021-10-25 to 2024-10-25."),
+    (1001, 2022-01-03, 2025-01-23, "The University of British Columbia will loan William Shakespeare's Comedies, Histories, & Tragedies to the 
+    museum from 2022-01-03 to 2025-01-23."),
+    (1002, 2022-07-02, 2025-07-02, "The Egyptian Museum will loan Tutankhamun's Mask and Papyrus of Ani - Book of the Dead to the museum from 
+    2022-07-02 to 2025-07-02."),
+    (1003, 2021-10-15, 2026-10-15, "The Royal BC Museum will loan Woolly mammoth replica to the museum from 2021-10-15 to 2026-10-15."),
+    (1004, 2023-02-04, 2026-02-04, "The Burnaby Village Museum will loan IBM cheese slicer to the museum from 2023-02-04 to 2026-02-04.")
+    (1005, 2023-10-28, 2024-10-28, "Richie Rich will loan Ancient Egyptian flint arrowhead to the museum from 2023-10-28 to 2024-10-28."),
+    (1006, 2021-09-07, 2025-09-07, "The Louvre Museum will loan Mona Lisa to the museum from 2021-09-07 to 2025-09-07."),
+    (1007, 2020-06-20, 2024-06-20, "The Museum of Modern Art will loan Starry Night replica to the museum from 2020-06-20 to 2024-06-20."),
+    (1008, 2023-03-05, 2027-03-15, "The National Museum of Art will loan The Scream replica to the museum from 2023-03-05 to 2027-03-15."),
+    (1009, 2022-10-17, 2025-10-17, "The University of British Columbia will loan Blue whale skeleton to the museum from 2022-10-17 to 2025-10-17");
 
--- Loans
+INSERT
+INTO Writes(ownerID, contractID)
+VALUES
+    (1111, 1000),
+    (1112, 1001),
+    (1113, 1002),
+    (1114, 1003),
+    (1115, 1004),
+    (1116, 1005),
+    (1117, 1006),
+    (1118, 1007),
+    (1119, 1008),
+    (1111, 1009);
 
--- PertainsTo
+INSERT
+INTO Loans(ownerID, articleID)
+VALUES
+    (1111, 11111),
+    (1112, 11112),
+    (1112, 11115),
+    (1113, 11114),
+    (1113, 11134),
+    (1114, 11116),
+    (1115, 11117),
+    (1116, 11119),
+    (1117, 11125),
+    (1118, 11126),
+    (1119, 11127);
 
--- Displays (Marcus fill in? depends on Exhibits)
+INSERT
+INTO PertainsTo(contractID, articleID)
+VALUES
+    (1000, 11111),
+    (1001, 11112),
+    (1002, 11114),
+    (1002, 11134),
+    (1003, 11116),
+    (1004, 11117)
+    (1005, 11119),
+    (1006, 11125),
+    (1007, 11126),
+    (1008, 11127),
+    (1009, 11115);
+
+-- Displays (depends on Exhibits TODO)
