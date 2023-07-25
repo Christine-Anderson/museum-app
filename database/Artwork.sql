ef47778b0afa730a255e22a5dfbcec89f5cefc49
Christine-Anderson <114715340+Christine-Anderson@users.noreply.github.com>
2023-07-24 20:38:50 -0700

    Update Artwork.sql constraints

--- database/Artwork.sql
+++ database/Artwork.sql
@@ -2,7 +2,7 @@ CREATE TABLE Artwork (
     articleID INT(11) PRIMARY KEY,
     artist VARCHAR(50),
     yearMade INT(10),
-    medium VARCHAR(50) NOT NULL,
+    medium VARCHAR(50),
     FOREIGN KEY(articleID) REFERENCES(Article)
         ON DELETE CASCADE
         ON UPDATE CASCADE
