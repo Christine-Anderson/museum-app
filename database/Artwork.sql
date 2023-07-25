45cd1ccd3acb69a7e6d649c39e63f300d38cbda8
Christine-Anderson <114715340+Christine-Anderson@users.noreply.github.com>
2023-07-24 20:25:02 -0700

    Update Artwork.sql integrity constraints

--- database/Artwork.sql
+++ database/Artwork.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Artwork (
     articleID INT(11) PRIMARY KEY,
     artist VARCHAR(50),
-    yearMade INT(10) NOT NULL,
+    yearMade INT(10),
     medium VARCHAR(50) NOT NULL,
     FOREIGN KEY(articleID) REFERENCES(Article)
         ON DELETE CASCADE
