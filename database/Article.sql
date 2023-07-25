f8773ea0ca8108fb675bf02bc309bc86cc2220ac
Christine-Anderson <114715340+Christine-Anderson@users.noreply.github.com>
2023-07-24 20:12:43 -0700

    Normalize Article and update integrity constraints

--- database/Article.sql
+++ database/Article.sql
@@ -1,8 +1,10 @@
 CREATE TABLE Article (
     articleID INT(11) PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255),
+    name VARCHAR(255) NOT NULL,
     dateAquired TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    condition VARCHAR(50),
-    storageLocation VARCHAR(50),
-    storageCondition VARCHAR(50)
+    condition VARCHAR(50) NOT NULL,
+    storageLocation VARCHAR(50) NOT NULL,
+    UVProtection VARCHAR(50) NOT NULL,
+    tempControl VARCHAR(50) NOT NULL,
+    humidityControl VARCHAR(50) NOT NULL
 );
