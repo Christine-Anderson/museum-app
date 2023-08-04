7c30a36da9fad4c37528ff302618ff111ec0d475
UBC Student <tineand@pender.students.cs.ubc.ca>
2023-08-03 17:15:37 -0700

    Add search queries to article information page
    Add search article location and storage conditions functionality
    Refactor code to abstract out shared functions and styling

---
+++ pages/archivist_article_info.php
@@ -0,0 +1,153 @@
+<!--
+    Title
+    Description TODO
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Archivist view article information page</title>
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+<body>
+    <?php
+    include 'archivist_sidebar.php'
+    ?>
+
+    <div class="content">
+        <h2>Search Article Information</h2>
+
+        <form method="GET" action="archivist_article_info.php">
+            <input type="hidden" id="article-info-request" name="article-info-request">
+            article ID: <input type="number" name="article-id" min="10000" max="99999" required>
+            <br /><br />
+
+            <label for="find">Find:</label>
+				<select name="find-option" id="find">
+					<option value="location">Location</option>
+					<option value="storage-condition">Storage Conditions</option>
+					<option value="article-detail">Article Details</option>
+				</select>
+			<br></br>
+
+            <input type="submit" value="Search" name="search-article-info"></p>
+        </form>
+
+        <p>TODO: sort articles by location</p>
+
+        <!-- <form method="POST" action="owner_profile.php">
+            <input type="hidden" id="update-profile-request" name="update-profile-request">
+            owner ID: <input type="number" name="article-id" min="1000" max="9999" required>
+            <br /><br />
+            Name: <input type="text" name="name"> <br /><br />
+            <input type="submit" value="Update" name="update-profile-submit"></p>
+        </form> -->
+            
+        <?php
+
+        include '../shared_functions/database_functions.php';
+        include '../shared_functions/print_functions.php';
+
+        // HANDLE ALL POST ROUTES
+        // function handlePOSTRequest() {
+        //     if (connectToDB()) {
+        //         if (array_key_exists('update-profile-submit', $_POST)) {
+        //             handleUpdateProfileRequest();
+        //         }
+        //         disconnectFromDB();
+        //     }
+        // }
+
+        // HANDLE ALL GET ROUTES
+        function handleGETRequest() {
+            if (connectToDB()) {
+                if (array_key_exists('search-article-info', $_GET)) {
+                    handleArticleInfoRequest();
+                }
+                disconnectFromDB();
+            }
+        }
+
+        // function handleUpdateProfileRequest() {
+        //     global $db_conn;
+
+        //     $article_id = $_POST['article-id'];
+        //     $name = $_POST['name'];
+
+        //     executePlainSQL(
+        //         "UPDATE owner 
+        //         SET name = '" . $name . "' 
+        //         WHERE owner_id = " . $article_id);
+
+        //     oci_commit($db_conn);
+
+        //     viewOwnerProfile($article_id);
+        // }
+
+        function handleArticleInfoRequest() {
+            global $db_conn;
+            $article_id = $_GET['article-id'];
+            $dropdown_value = $_GET['find-option'];
+
+            if ($dropdown_value == 'location') {
+                getArticleLocation($article_id);
+            } else if ($dropdown_value == 'storage-condition') {
+                getArticleStorageConditions($article_id);
+            } else { // $dropdown_value == 'article-detail'
+                getArticleDetails($article_id);
+            }
+        }
+
+        function getArticleLocation($article_id) {
+            global $db_conn;
+
+            $result = executePlainSQL(
+                "SELECT article_id, name, storage_location
+                FROM article
+                WHERE article_id = " . $article_id . "");
+
+            printResults($result);
+        }
+
+        function getArticleStorageConditions($article_id) {
+            global $db_conn;
+
+            $result = executePlainSQL(
+                "SELECT article_id, name, uv_protection, temp_control, humidity_control
+                FROM article
+                WHERE article_id = " . $article_id . "");
+
+            printResults($result);
+        }
+
+        function getArticleDetails($article_id) {
+            global $db_conn;
+
+            $result = executePlainSQL( //fix me come back to this one
+                "SELECT *
+                FROM article");
+                // FROM article a, artwork art, text t, photo p, artifact old, naturalspecimen ns, species s
+                // WHERE   a.article_id = " . $article_id . ") AND
+                //         a.article_id = art.article_id OR
+                //         a.article_id = t.article_id OR
+                //         a.article_id = p.article_id OR
+                //         a.article_id = old.article_id OR
+                //         (a.article_id = ns.article_id AND
+                //         ns.species_name = s.species_name)");
+
+            printResults($result);
+        }
+
+
+
+        if (isset($_POST['update-profile-request'])) {
+            handlePOSTRequest();
+        } else if (isset($_GET['article-info-request'])) {
+            handleGETRequest();
+        }
+        ?>  
+    </div>
+</body>
+</html>
---
+++ shared_functions/print_functions.php
@@ -0,0 +1,50 @@
+<!--
+    Print functions for outputting database search results to html table
+-->
+
+<?php
+
+function printResults($result){ 
+	if (oci_num_fields($result) > 0) {
+        printTable($result);
+    } else {
+        echo "No results found";
+    }
+}
+
+function printTable($result){ 
+	global $db_conn;
+	echo "<table>";
+	echo "<tr>";
+
+	$ncols = oci_num_fields($result);
+
+    for ($i = 1; $i <= $ncols; $i++) {
+        echo "<th>" . convertToTitleCase(oci_field_name($result, $i)) . "</th>";
+    }
+
+    echo "</tr>";
+
+    while ($row = oci_fetch_assoc($result)) {
+        echo "<tr>";
+        foreach ($row as $value) {
+            echo "<td>" . $value . "</td>";
+            } 
+        echo "</tr>";
+	}
+
+	echo "</table>";
+}
+
+function convertToTitleCase($string) {
+    if ($string) {
+        // split by delimiter
+        $word_arr = explode("_", $string);
+        // convert each word to title case
+        $word_arr = array_map('strtolower', $word_arr);
+        $word_arr = array_map('ucfirst', $word_arr);
+        // join array to string
+        return implode(" ", $word_arr);
+    }
+}
+?>
