50848b5e806746c96a839e72263d15562e9c0e25
aradprsabet <aradpr@students.cs.ubc.ca>
2023-08-08 12:40:10 -0400

    added pages

---
+++ pages/frontdesk_events.php
@@ -0,0 +1,20 @@
+
+
+<!--
+    Front desk provides viws exhibits and activits
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Front desk display article page</title>
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+<body>
+    <?php
+    echo "Successfully redirected to viewing page.";
+    ?>
+</body>
+</html>
