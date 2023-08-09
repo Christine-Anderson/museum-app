98ce87a451c0401dc672384c3f5db97b9d475dfb
UBC Student <tineand@pender.students.cs.ubc.ca>
2023-08-08 19:59:46 -0700

    Add delete article funcitonality

--- pages/debug_wrapper.php
+++ pages/debug_wrapper.php
@@ -8,8 +8,8 @@
 	error_reporting(-1);
 	ini_set('display_errors',1);
 	// include("archivist_display_article.php");
-    // include("archivist_examine_article.php");
-	include("archivist_search_article.php");
+    include("archivist_examine_article.php");
+	// include("archivist_search_article.php");
 	// include("employee_login.php");
 	// include("employee_redirect.php");
 ?>
